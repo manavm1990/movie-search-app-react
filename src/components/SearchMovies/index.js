9f4c9bd3a4baea694a9be70894b65fdef6b00e28
Manav Misra <manavm@visionwebsoft.com>
2020-07-17 11:38:36 -0500

    :construction: Use dotenv to log 🔑

--- src/components/SearchMovies/index.js
+++ src/components/SearchMovies/index.js
@@ -1,9 +1,20 @@
 import React from "react";
 
+import dotenv from "dotenv";
+
 import styles from "./SearchMovies.module.css"; // Import css modules stylesheet as styles
 
+dotenv.config();
+
+const searchMovies = async (event) => {
+  event.preventDefault();
+
+  const url = `https://api.themoviedb.org/3/movie/550?api_key=${process.env.REACT_APP_API_KEY}`;
+  console.log(url);
+};
+
 export const SearchMovies = () => (
-  <form className={styles.form}>
+  <form className={styles.form} onSubmit={searchMovies}>
     <label htmlFor="query" className={styles.label}>
       Movie Name
     </label>
