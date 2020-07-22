2a1973e69693c9b50c4f415ca6ddd88a36446e67
Manav Misra <manavm@visionwebsoft.com>
2020-07-21 20:00:44 -0500

    :card_file_box: Set up API to fetch and return json/error

--- src/api/index.js
+++ src/api/index.js
@@ -6,9 +6,17 @@ export default (
   baseURL = "https://api.themoviedb.org/3/search/movie",
   key = process.env.REACT_APP_API_KEY
 ) => ({
-  async query(query) {
-    return await fetch(
-      `${baseURL}?api_key=${key}&query=${query}&page=1&include_adult=false`
-    );
+  async index(query) {
+    try {
+      const res = await fetch(
+        `${baseURL}?api_key=${key}&query=${query}&page=1&include_adult=false`
+      );
+      if (res.status > 400) {
+        throw new Error(res.status);
+      }
+      return await res.json();
+    } catch (error) {
+      return `Currently facing issue with: ${error.message}`;
+    }
   },
 });
